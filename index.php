88ca62da8407f2fbeff2739b74f8f8c69e05c6aa
Nitraatss <nitraatss@gmail.com>
2017-11-12 19:15:20 +0300

    test

--- index.php
+++ index.php
@@ -2,17 +2,13 @@
 // показывать или нет выполненные задачи
 $show_complete_tasks = rand(0, 1);
 // устанавливаем часовой пояс в Московское время
-
 date_default_timezone_set('Europe/Moscow');
 $days = rand(-3, 3);
 $task_deadline_ts = strtotime("+" . $days . " day midnight"); // метка времени даты выполнения задачи
 $current_ts = strtotime('now midnight'); // текущая метка времени
 $date_deadline = date("d.m.Y",$task_deadline_ts);//дата выполнения задачи
 $days_until_deadline = floor(($task_deadline_ts - $current_ts) / 86400); //кол-во дней до даты задачи
-
-
 $projects = ["Все", "Входящие", "Учеба", "Работа", "Домашние дела", "Авто"]; //массив проектов
-
 //массив задач
 $tasks = [
 [
