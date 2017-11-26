3645de67714123b62fd0d019d7d42fb1d6f7af00
Nitraatss <nitraatss@gmail.com>
2017-11-26 13:12:59 +0300

    Установлен размер char

--- schema.sql
+++ schema.sql
@@ -4,19 +4,19 @@ USE schema_db;
 
 CREATE TABLE projects(
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-category CHAR
+category CHAR(50)
 );
 
 CREATE TABLE users(
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-email CHAR,
-username CHAR,
-pass CHAR
+email CHAR(255),
+username CHAR(50),
+pass CHAR(60)
 );
 
 CREATE TABLE tasks(
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-title CHAR,
+title CHAR(255),
 deadline_date DATE,
 is_done BOOLEAN,
 project_id INT,
