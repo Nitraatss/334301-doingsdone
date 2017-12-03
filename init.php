803b845cb3b4cecedc9cd4bdba1284c7b9366241
Nitraatss <nitraatss@gmail.com>
2017-12-03 08:52:52 +0300

    Внесены правки

--- init.php
+++ init.php
@@ -3,16 +3,15 @@ $db_host = "localhost";
 $db_login = "root";
 $db_password = "";
 $db_name = "schema_db";
-$db_error;
 
-$res = mysqli_connect($db_host, $db_login, $db_password, $db_name);
+$db_link = mysqli_connect($db_host, $db_login, $db_password, $db_name);
 
-if ($res == false)
+if ($db_link == false)
 {
-    $db_error = mysqli_connect_error;
+    $db_error = mysqli_connect_error();
     
     $page_content = include_template("templates/error.php", [
-    "db_error" => $db_error
+        "db_error" => $db_error
     ]);
     
     print($page_content);
